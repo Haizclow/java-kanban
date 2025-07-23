18e6e21a741b65acfc8e81ae64a226df8f3be1e8
Victor Avramenko <viktor.avramenkl@yandex.ru>
2025-07-24 00:01:33 +0300

    фз8

--- src/com/yandex/app/service/TaskManager.java
+++ src/com/yandex/app/service/TaskManager.java
@@ -1,35 +1,49 @@
 package com.yandex.app.service;
 
-import com.yandex.app.model.*;
-
+import com.yandex.app.model.Epic;
+import com.yandex.app.model.Subtask;
+import com.yandex.app.model.Task;
 import java.util.List;
 
 public interface TaskManager {
+
     int addTask(Task task);
+
     int addEpic(Epic epic);
+
     int addSubtask(Subtask subtask);
 
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
+
     List<Subtask> getEpicSubtasks(int epicId);
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     void deleteTasks();
+
     void deleteEpics();
+
     void deleteSubtasks();
 
     List<Task> getHistory();
-    List<Task> getPrioritizedTasks();
 }
